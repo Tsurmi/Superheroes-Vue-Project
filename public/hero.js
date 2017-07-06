4bbc00201527d0b9b06488ff89d06c75890708d6
Travis Surmi <travissurmi@Traviss-MacBook-Pro.local>
2017-07-06 12:07:49 -0700

    added forms/button/image to our superheroes page

--- public/hero.js
+++ public/hero.js
@@ -1,11 +1,14 @@
 var title = "Superheroes";
-
-
+var appUrl = "/api"
 var app = new Vue({
   el: "#app",
   data: {
     title: title,
     heroes: undefined,
+    postTitle: "Create a Superhero",
+    name: undefined,
+    superpower: undefined,
+    img: undefined
 
   },
   created: function(){
@@ -21,6 +24,22 @@ var app = new Vue({
         self.heroes = response.data;
         console.log("received heroes", self.heroes);
     })
+    },
+    postHero: function(){
+      var self = this;
+      var newSuperHero = {
+        name: this.name,
+        superpower: this.superpower,
+        img: this.img
+      };
+      console.log("newSuperHero");
+      $.ajax({
+        url:'/api',
+        method:'POST',
+        data: newSuperHero
+      }).done(function(response){
+        console.log(response.data, "Hero Created");
+      });
     }
   }
 });
--- server.js
+++ server.js
@@ -31,14 +31,17 @@ app.get('/api/:_id', function(req,res){
 });
 
 app.post('/api', function(req, res) {
+  console.log("Hitting post route");
   var superhero = new Superhero();
   superhero.name = req.body.name;
   superhero.superpower = req.body.superpower;
+  superhero.img = req.body.img;
 
   superhero.save().then(function(superhero){
-    res.send(superhero);
+    res.json({message:"hero succesfully created", data: superhero});
   }, function(err) {
-    res.send(err);
+    res.send("Failed to save");
+
   });
 });
 var server = app.listen(port, function(){
