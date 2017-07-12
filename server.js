bb343ba9a67d7fb1c79ee8f6ff6a3d1128be2a2b
Travis Surmi <travissurmi@Traviss-MacBook-Pro.local>
2017-07-12 09:29:49 -0700

    added a new route to our superheroes_vue

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var Superhero = require('./models/Superhero');
+var mainRoutes = require('./routes/main');
+var Superhero = require('./models/Superhero'); //node will assume that files are js
+var heroRoutes = require('./routes/superheroes');
 var app = express();
 var port = 3003;
 
@@ -9,9 +11,11 @@ mongoose.connect('mongodb://localhost/superheroes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-
+app.use("/", mainRoutes);
+app.use("/api/heroes", heroRoutes);
 //links the html,css and js files together
 app.use(express.static(__dirname + '/public'));
+
 
 
 app.get('/api/heroes', function(req,res){
